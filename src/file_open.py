1ae3c777b919df2203c15097280c9e6910387c54
Luis Murta <luismurta05@gmail.com>
2017-10-10 13:54:41 +0100

    Project description added and file reader updated

--- src/file_open.py
+++ src/file_open.py
@@ -2,6 +2,21 @@ tests_path = '../test/'
 
 with open(tests_path + 'example.txt') as fd:
 	lines = [line.strip().split(' ') for line in fd]
-	
-for x in lines:
-	print(x)
+
+for line in lines:
+	if line[0][0]=='V':
+		V_id = line[0]
+		weight = line[1]
+	elif line[0][0]=='E':
+		V_id = line[0]
+		Vid1 = line[1]
+		Vid2 = line[2]
+	elif line[0][0]=='L':
+		date = line[1]
+		max_payload = line[2]
+		fixed_cost = line[3]
+		variable_cost = line[4]
+	else:
+		print(line)
+
+print(stop-start)
