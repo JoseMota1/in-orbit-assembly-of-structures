28c63ff3eb1671ee3f641c79ece7deaf69fb1c92
Inês Afonso <afonso_ines@hotmail.com>
2017-10-17 15:48:02 +0100

    Added Frontier class. WIP

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,11 +1,33 @@
-from collections import namedtuple, deque
+from collections import namedtuple, deque # lista imutavel com acesso hash , lista duplamente ligada
 from itertools import combinations
 
-Vertice = namedtuple('Vertice', ['id', 'weight'])
+Vertice = namedtuple('Vertice', ['id', 'weight']) 
 Launch = namedtuple('Launch', ['date', 'payload', 'fixed_cost', 'variable_cost'])
 
+class Frontier:
+	def __init__(self):
+		self.queue = list()
+		self.visited = dict()
+
+	def insert(self, node):
+		if not visited[node.state]
+			heapq.heappush(self.queue, node)
+			visited[node.state] = node
+		if visited[node]
+			if node.cost < visited[node].cost
+				heapq.heappush(self.queue, node)
+
+	def pop(self):
+		while True:
+			node = heapq.heappop(self.queue)
+			if not visited[node]:
+				return node
+
+
+
+
 class Launch:
-	__slots__ = ('date', 'max_payload', 'fixed_cost', 'variable_cost')
+	__slots__ = ('date', 'max_payload', 'fixed_cost', 'variable_cost') 
 	def __init__(self, date, max_payload, fixed_cost, variable_cost):
 		self.date = date
 		self.max_payload = max_payload
@@ -20,7 +42,8 @@ class Launch:
 		
 		
 class State:
-	
+
+	__slots__ = ('land', 'air', 'date')
 	def __init__(self, land, air, date):
 		self.land = land
 		self.air = air
@@ -30,7 +53,7 @@ class State:
 class Node:
 	__slots__ = ('state', 'cost', 'parent', 'action')
 	def __init__(self, state, cost, parent, action):
-		self.state = state
+		self.state = state 
 		self.parent = parent
 		self.action = action
 		self.cost = cost
@@ -44,15 +67,15 @@ class Problem:
 		self.launches = launches		
 		
 	def actions(self, state):
-		max_payload = self.launches[state.date]
+		max_payload = self.launches[state.date].max_payload #estamos a assumir um unico lançamento por data
 		
 		# TODO still need to check weight
 		actions = []
-		nvertices = 3 # better checking
+		nvertices = 3 # better checking TODO
 		for vertice in land:
-			actions.append('load' + vertice.id)
+			# actions.append('load' + vertice.id)
 			connections = (v for v in edges[vertice] if v not in air)
-			[actions.append( 'load ' + vertice.id + ' ' +  str(vs)) for n in nvertices for vs in combinations(childs, n) if vs if sum(v.weight for v in vs) < max_payload]
+			[actions.append( 'load ' + vertice.id + ' ' +  str(vs)) for n in range(nvertices+1) for vs in combinations(connections, n) if vs if sum(v.weight for v in vs) < max_payload]
 			
 		return actions
 		
--- src/satelites.py
+++ src/satelites.py
@@ -4,7 +4,7 @@ from datastructures import *
 with open(test_path + 'example.txt') as fd:
 	lines = [line.strip().split(' ') for line in fd]
 	
-lines.sort(reverse=True)
+lines.sort(reverse=True) #ordem alfabetica ao contrario V L E
 #print(lines)
 vertices = dict()
 edges = dict()
@@ -21,8 +21,8 @@ for line in lines:
 		edges.setdefault(v1, []).append(v2)
 		edges.setdefault(v2, []).append(v1)
 	elif line[0]=='L':
-		l = Launch(line[1], line[2], line[3], line[4])
-		launches.setdefault(line[1], []).append(l)
+		launches[line[1]] = Launch(line[1], line[2], line[3], line[4])
+
 
 		
 #print(vertices)
