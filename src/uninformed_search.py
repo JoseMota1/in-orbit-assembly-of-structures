1b06d00824ccd9759b51b03d83dcfd1d9f959020
Luis Murta <luismurta05@gmail.com>
2017-10-21 15:31:31 +0100

    Updated uninformed search algorithm to use the correct Problem functions

--- src/uninformed_search.py
+++ src/uninformed_search.py
@@ -1,43 +1,24 @@
 def cost_search(problem)
 
-	node=a;
+	node=Node();
 	PATH_COST = 0;
 	explored=[];
-	ordered_front=heapsort(frontier) #a priority queue ordered by PATH-COST, with node as the only elemen
+	frontier=Frontier() #a priority queue ordered by PATH-COST, with node as the only elemen
 
-	while (len(ordered_front) > 0)
-		if len(frontier) == 0
+	while True:
+		if not frontier:
 			return failure
 
 		node = heapq.heappop(frontier)	# chooses the lowest-cost node in frontier (first)
+		if problem.goal(node.state):
+			return problem.solution(node)
 
-		if problem.GOAL_TEST(node.STATE)
-			explored.append(node.STATE);
+		explored.append(node.state);
 
-		for each_action in problem.ACTIONS(node.STATE)
-			child = CHILD_NODE(problem, node, action);
-			if (present_state(explored,child)  || present_state(frontier, child)) && (child.path_cost < node.path_cost)
-				frontier.append(child);
-			elif present_state(frontier,child) && child.path_cost > node.path_cost)
+		for action in problem.actions(node.state):
+			child = problem.childnode(node, action)
+			# TODO lower code is done in the Frontier class
+			if child.state in explored  or (child.state in frontier and child.path_cost < node.cost):
+				frontier.insert(child)
+			elif child.state in frontier and child.cost > node.cost): # if coiso in ...
 				frontier = child;
-	return result
-
-
-def present_state(lista,node)
-i=0;
-	while i<= len(lista)
-		if lista[i] == node.STATE;
-			return 1;
-			break
-		else
-			return 0;
-	i=i+1;
-
-
-def CHILD_NODE(problem, parent , action)
-
-	node.STATE = RESULT(parent.STATE, action),
-	node.PARENT = parent
-	ACTION = action
-	node.PATH_COST = parent.PATH_COST + problem.STEP_COST(parent.STATE, action)
-return node
